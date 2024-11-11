0f6cb32acdc109b26074b6f0cc7670723cb7979f
Sangjin Choi <sjchoi@casys.kaist.ac.kr>
2024-11-11 17:04:45 +0900

    pipeline parallelism init

---
+++ scripts/run_docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+workspace=$(dirname $(dirname $(realpath $0)))
+
+docker_tag="nvcr.io/nvidia/pytorch:23.10-py3"
+
+# Function to find an available port, starting from a given port
+find_available_port() {
+  local start_port=$1
+  while :; do
+    # Check if the port is in use by any process (using lsof) or Docker container
+    if ! lsof -i :"$start_port" &>/dev/null && ! docker ps --format '{{.Ports}}' | grep -q ":$start_port->"; then
+      echo "$start_port"
+      return
+    fi
+    start_port=$((start_port + 1))
+  done
+}
+
+# Set the initial port number and find an available port
+initial_port=8000
+port=$(find_available_port "$initial_port")
+
+# Run Docker container with the selected tag and available port
+docker run --gpus all -it --rm \
+--ipc host \
+--ulimit memlock=-1 \
+--ulimit stack=67108864 \
+--shm-size=12g -p "$port:$port" \
+-v "$workspace":"$workspace" \
+-w "$workspace" \
+"$docker_tag"
